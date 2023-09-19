567a33199ca4cb52c6db026b059944973106c14b
whatever <voronov.xcvi@gmail.com>
2023-09-19 21:25:31 +0300

    app v

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { Provider } from "react-redux";
 import NavTree from "./src/NavTree";
 import { store } from "./src/store";
+import "react-native-reanimated";
 
 export default function App() {
   return (
